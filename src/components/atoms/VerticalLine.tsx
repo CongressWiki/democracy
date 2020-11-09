dd6f1bb6a11157e3b4f3bed89a1c2a06ff59bb87
Ryan Parker <ryan.parker3@outlook.com>
2020-11-09 09:50:37 -0600

    Starting fresh

--- src/components/atoms/VerticalLine.tsx
+++
@@ -1,5 +0,0 @@
-import styles from '../../styles/VerticalLine.module.css';
-
-export default function VerticalLine() {
-	return <div className={styles.vLineInCenter} />
-}
--- src/pages/api/hello.ts
+++
@@ -1,7 +0,0 @@
-import {NextApiRequest, NextApiResponse} from 'next';
-
-const hello = (_: NextApiRequest, response: NextApiResponse) => {
-	response.status(200).json({text: 'Hello'});
-};
-
-export default hello;
