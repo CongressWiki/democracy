c16efec91e7c0c7d4e3e39e02e1399250e084654
Ryan Parker <ryan.parker3@outlook.com>
2020-11-22 16:18:20 -0600

    Added Storybook

--- front-end/components/bill-canvas.tsx
+++ front-end/components/bill-canvas.tsx
@@ -12,7 +12,7 @@ const useStyles = makeStyles({
 	root: {
 		// Position: 'absolute',
 		display: 'inline-block',
-		background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+		background: 'linear-gradient(45deg, #2979ff 30%, #f44336 90%)',
 		border: 1,
 		borderRadius: 3,
 		boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
