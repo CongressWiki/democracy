985bb1b3d57f2e013edf2caff98870576ed8acce
Ryan Parker <ryan.parker3@outlook.com>
2020-11-22 18:21:53 -0600

    style(components/): kabal -> pascal filenames

--- front-end/pages/index.tsx
+++ front-end/pages/index.tsx
@@ -1,5 +1,5 @@
-import BillCanvas from '../components/bill-canvas';
-import Layout from '../components/layout';
+import BillCanvas from '../components/BillCanvas';
+import Layout from '../components/Layout';
 import React from 'react';
 import {withApollo} from '../lib/with-apollo';
 
